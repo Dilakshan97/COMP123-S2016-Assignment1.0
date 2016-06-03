00b52ca23e0d49a1046e2125ec584cd7888cf77f
Dilakshan Packiyanathan <dilaxan.super@hotmail.com>
2016-06-03 11:19:34 -0400

    Created a Hero Class

---
+++ COMP123-S2016-Assignment1.0/Hero.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *Author: Dilakshan Packiyanathan
+ *Last Modified on June 3rd 2016
+ *Program Description: Create a Hero class with strength, speed, health and name properties. Assign a random 
+ *                     number between 1 and 100 to the abilities.
+ *                     determine the damage if hero hits the target and display appropriate messages to the 
+ *                     console. 
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Assignment1._0
+{
+    class Hero
+    {
+    }
+}
